1135b55c2e8692e3ae7335545577c9e6cd97c5da
ang-zhao <azhaowork@gmail.com>
2026-03-15 16:47:18 -0400

    Fixed code

--- hw5/gpt/model.py
+++ hw5/gpt/model.py
@@ -68,11 +68,14 @@ class CausalSelfAttention(nn.Module):
         # we provide the implementation for the key projections as an example
 
         # similarly, implement the query and value projections
-
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
 
         # causal self-attention; Self-attend: (B, n_head, T, n_embd / n_head) x (B, n_head, n_embd / n_head, T) -> (B, n_head, T, T)
         # calculate the scaled dot-product attention with causal mask, namely the attention matrix as `att`
         # step 1: q @ k^T / sqrt(d_k), where d_k is the head hidden dimension (n_embd / n_head)
+        att = (q @ k.transpose(-2, -1)) / math.sqrt(C // self.n_head)
 
         # step 2: apply the causal mask to the attention matrix
         # the masked out entries in att should have the value of float('-inf')
@@ -81,14 +84,18 @@ class CausalSelfAttention(nn.Module):
         # - don't forget to truncate the mask to the actual sequence length (T)
         # - to apply the mask, one possible way is the `torch.Tensor.masked_fill` method
         # - - for this, you can obtain the boolean mask by element-wise comparison of the causal mask with 0
+        att = att.masked_fill(self.causal_mask[:, :, :T, :T] == 0, float('-inf'))
 
         # step 3: apply the softmax function to the masked attention matrix
         # hint: you can use the `F.softmax` function
+        att = F.softmax(att, dim=-1)
 
         # step 4: apply the attention dropout (self.attn_dropout) to the attention matrix
+        att = self.attn_dropout(att)
 
         # step 5: multiply the attention matrix with value vectors
-
+        y = att @ v
+        
         # your code ends here
 
         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
